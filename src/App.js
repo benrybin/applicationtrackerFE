482c7013d2eaa503b6a9441e6ac021215797d49b
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2019-09-14 11:56:20 -0400

    early demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    groups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:8080/jobapp/all');
+    const body = await response.json();
+    this.setState({ groups: body, isLoading: false });
+    
+  }
+
+  render() {
+    const {groups, isLoading} = this.state;
+    console.log(groups);
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2> List</h2>
+            {groups.map(group =>
+              <div key={group.companyName}>
+                {group.companyName}
+              </div>
+              
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
